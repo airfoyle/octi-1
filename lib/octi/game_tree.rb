9eeea4daed2b828eb7e5e0b88f50dcf980bff3a7
kboateng3 <kboateng3@gmail.com>
2014-04-28 11:12:54 -0400

    can move

--- lib/octi/game_tree.rb
+++ lib/octi/game_tree.rb
@@ -17,21 +17,43 @@ module Octi
 			x.each_with_index do |game_state.current_player, position|
 			unless player_at_position #base case placeholder
 
-				if game_state.board
-				game_state.current_player.pods.each do |pod|
-					pod.prongs.each do |prong|
-						prong ? prong = false : prong = true
-						next_board = game_state.board.dup
-						next_board[position] = #game_state.current_player
-
+				if game_state.board[x][position] == nil # is this necessary?
+					@board.each do |col|
+						col.each do |cell|
+							if cell.is_a?(Pod)
+								pod = cell
+								if pod.player == comp
+									if pod.can_move?(@board,x,position, col,cell)
+										next_board = game_state.board.dup
+										next_board[col][cell] = nil
+										next_board[x][position] = pod
+										next_board = game_state.board.dup
+										next_game_state = (GameState.cache.states[next_baord] 
+											||= GameState.new(next_player, next_board)))
+										game_state.moves << next_game_state
+										generate_moves(next_game_state)										
+									end
+								else
+									pod.prongs.each do |prong|
+										if !prong 
+											prong = true
+											next_board = game_state.board.dup
+											next_board[x][position] = #game_state.current
+											next_board = game_state.board.dup
+											next_game_state = (GameState.cache.states[next_baord] 
+												||= GameState.new(next_player, next_board)))
+											game_state.moves << next_game_state
+											generate_moves(next_game_state)	
+										end	
+									end						
+								end
+							end
+						end
 					end
 				end
-
-				#if prong exists
 				next_board = game_state.board.dup
-				#next_board[position] = game_state.current_player
-
-				next_game_state = (GameState.cache.states[next_baord] ||= GameState.new(next_player, next_board)))
+				next_game_state = (GameState.cache.states[next_baord] 
+					||= GameState.new(next_player, next_board)))
 				game_state.moves << next_game_state
 				generate_moves(next_game_state)
 			end
--- lib/octi/pod.rb
+++ lib/octi/pod.rb
@@ -8,7 +8,26 @@ module Octi
 			self.player = player
 		end
 
-		def can_move?
+		def can_move?(board,x2,y2,x1,y2)
+			 dx - x2 - x1
+			 dy = y2 - y1
+
+			 #is destination in range?
+			 if dx.abs > 1 || dy.abs > 1
+			 	return false #can jump?
+			 end
+
+			 count = -1
+			 for i in -1..1
+			 	for j in -1..1
+			 		count++
+			 		if (0..5)===(x1+i) && (0..6)===(y1+j) #on board
+			 			if @prongs[count] 
+			 				return true
+			 			end
+			 		end
+			 	end
+			 end
 		end
 
 		def can_jump?
